3e8c0d86ada84d2b5f8ba7a153a4e3d468406c90
felipedorea <122095865+felipedorea@users.noreply.github.com>
2024-04-06 12:48:29 -0300

    .

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,9 @@
 function abrirMenu(){
     let menuMobile = document.getElementById("mobil");
     menuMobile.classList.toggle("abrir");
+
+    document.getElementById("icone").classList.toggle("fa-x");
+    
 };
+
+
