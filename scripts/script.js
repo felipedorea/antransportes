1ac6165be32c885a6230347fd88b1c6c375a887d
felipedorea <122095865+felipedorea@users.noreply.github.com>
2024-04-06 11:12:23 -0300

    responsividade

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+// let menu = document.getElementById("navlist-mobile");
+// let barra = document.getElementById("barras").addEventListener("click", abrirMenu);
+
+// function abrirMenu(){
+//     menu.style.display = "flex";
+// };
