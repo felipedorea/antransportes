84ce4acd382d9ecb40639e6c56dc3d60351fd8e3
felipedorea <122095865+felipedorea@users.noreply.github.com>
2024-04-12 11:04:09 -0300

    ..

---
+++ scripts/modslist.js
@@ -0,0 +1,33 @@
+const modsList = [
+    {
+        id: 1,
+        img: "../assets/imagens/imgmod",
+        name: "Mod 1",
+        categorie: "Caminhões",
+        description: "Descrição do mod",
+    },
+
+    {
+        id: 2,
+        img: "../assets/imagens/imgmod",
+        name: "Mod 2",
+        categorie: "Caminhões",
+        description: "Descrição do mod",
+    },
+
+    {
+        id: 3,
+        img: "../assets/imagens/imgmod",
+        name: "Mod 3",
+        categorie: "Caminhões",
+        description: "Descrição do mod",
+    },
+
+    {
+        id: 4,
+        img: "../assets/imagens/imgmod",
+        name: "Mod 4",
+        categorie: "Caminhões",
+        description: "Descrição do mod",
+    }
+]
