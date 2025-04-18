58ad4f9aed755cb9a3d159044da069f84d48a232
felipedorea <122095865+felipedorea@users.noreply.github.com>
2025-04-17 21:39:56 -0300

    faq

--- scripts/script.js
+++ scripts/script.js
@@ -32,4 +32,29 @@ function up(){
     }
 }
 
+let accordion1 = document.getElementById("title1").addEventListener("click", () => {
+  document.getElementById("chevron1").classList.toggle("chevron");
+  document.getElementById("content1").classList.toggle("active");
+})
+
+let accordion2 = document.getElementById("title2").addEventListener("click", () => {
+  document.getElementById("chevron2").classList.toggle("chevron");
+  document.getElementById("content2").classList.toggle("active");
+})
+
+let accordion3 = document.getElementById("title3").addEventListener("click", () => {
+  document.getElementById("chevron3").classList.toggle("chevron");
+  document.getElementById("content3").classList.toggle("active");
+})
+
+let accordion4 = document.getElementById("title4").addEventListener("click", () => {
+  document.getElementById("chevron4").classList.toggle("chevron");
+  document.getElementById("content4").classList.toggle("active");
+})
+
+let accordion5 = document.getElementById("title5").addEventListener("click", () => {
+  document.getElementById("chevron5").classList.toggle("chevron");
+  document.getElementById("content5").classList.toggle("active");
+})
+
 
